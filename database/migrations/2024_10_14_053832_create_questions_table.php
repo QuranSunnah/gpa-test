028344203f6676f928187d22b02c55d506b00fa8
Sajal Ahmed <sazal.me@gmail.com>
2024-10-15 13:05:11 +0600

    add question bank, migraiton and model

---
+++ database/migrations/2024_10_14_053832_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->char('title', 255);
+            $table->integer('category_id');
+            $table->tinyInteger('question_type')->comment('1=text,2=single,3=multiple');
+            $table->json('options')->nullable();
+            $table->char('answer', 255)->nullable();
+            $table->json('feedbacks')->nullable();
+            $table->integer('time_limit')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
