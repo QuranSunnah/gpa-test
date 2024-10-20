28b2b4210a629874657603c6a52f45bcdfca8169
Sajal Ahmed <sazal.me@gmail.com>
2024-10-20 12:27:45 +0600

    add is_higlighted column in category tble

--- database/migrations/2024_10_09_104109_create_categories_table.php
+++ database/migrations/2024_10_09_104109_create_categories_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->char('image', 255)->nullable();
             $table->integer('order')->default(0);
             $table->boolean('is_top')->default(1);
+            $table->boolean('is_highlighted')->default(0);
             $table->json('others')->nullable();
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
