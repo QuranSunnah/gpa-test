4c1d2ae276ab2c70e4c80d2a6b26c7a1f8107475
Sajal Ahmed <sajal.ahmed@brainstation-23.com>
2024-10-21 12:57:18 +0600

    add comment to category migrations

--- database/migrations/2024_10_09_104109_create_categories_table.php
+++ database/migrations/2024_10_09_104109_create_categories_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->boolean('is_top')->default(1);
             $table->boolean('is_highlighted')->default(0);
             $table->json('others')->nullable();
-            $table->tinyInteger('status')->default(config('common.status.active'));
+            $table->tinyInteger('status')->default(config('common.status.active'))->comment('1=active,0=inactive');
             $table->timestamps();
             $table->softDeletes();
         });
