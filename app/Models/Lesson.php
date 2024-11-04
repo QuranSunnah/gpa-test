a173ffe752b3a1716480f39355ad2f7b75028e27
Md. Razu Haolader <razu.haolader@brainstation-23.com>
2024-11-04 17:18:17 +0600

    course detail API

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -6,8 +6,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Lesson extends Model
 {
     use HasFactory;
+
+    public function section(): BelongsTo
+    {
+        return $this->belongsTo(Section::class);
+    }
+
+    public function contentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
