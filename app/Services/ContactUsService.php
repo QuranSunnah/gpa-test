703b3347b08bb9625265840e34af67c73a970430
Sajal Ahmed <sajal.ahmed@brainstation-23.com>
2025-01-30 17:51:33 +0600

    add controller and serveri contact

---
+++ app/Services/ContactUsService.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Repositories\LessonRepository;
+use Illuminate\Http\Response;
+
+class ContactUsService
+{
+
+    public function save() {}
+}
