2a177ab8c95551f233bb65b9c5c24ce264818ea5
Sajal Ahmed <sajal.ahmed@brainstation-23.com>
2025-01-30 17:51:43 +0600

    add controller and serveri contact

--- app/Http/Controllers/Api/V1/ContactUsController.php
+++ app/Http/Controllers/Api/V1/ContactUsController.php
@@ -1,11 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class ContactUsController extends Controller
 {
-    //
 }
--- app/Services/ContactUsService.php
+++ app/Services/ContactUsService.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Repositories\LessonRepository;
-use Illuminate\Http\Response;
-
 class ContactUsService
 {
-
-    public function save() {}
+    public function save()
+    {
+    }
 }
