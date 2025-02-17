824939537f05cc51a53aae6cae45a7932c71b77a
Sajal Ahmed <sajal.ahmed@brainstation23.com>
2025-02-17 11:48:52 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::group(['prefix' => 'v1'], function () {
     Route::get('/news/{slug}', [Api\V1\NewsController::class, 'show']);
     Route::get('/events', [Api\V1\EventsController::class, 'index']);
     Route::get('/events/{slug}', [Api\V1\EventsController::class, 'show']);
-    Route::get('/gallery', [Api\V1\GalleryController::class, 'index']);
+    Route::get('/galleries', [Api\V1\GalleryController::class, 'index']);
     Route::get('/galleries/{slug}', [Api\V1\GalleryController::class, 'show']);
     Route::get('/courses', [Api\V1\CourseController::class, 'index']);
     Route::get('/courses/{slug}', [Api\V1\CourseController::class, 'show']);
