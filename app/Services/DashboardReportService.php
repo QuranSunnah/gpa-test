2ec3178652276570ac631fd7e3388a54343b519d
Sajal Ahmed <sajal.ahmed@brainstation-23.com>
2025-04-16 16:37:23 +0600

    dashboard report refactor

--- app/Services/DashboardReportService.php
+++ app/Services/DashboardReportService.php
@@ -31,29 +31,29 @@ class DashboardReportService
                 ->whereBetween('enrolls.created_at', [$start, $end])
                 ->whereNull('enrolls.deleted_at')
                 ->groupBy(['date', 'gender'])
-                ->get()->groupBy([fn ($item) => $item->date, fn ($item) => $item->gender]),
+                ->get()->groupBy([fn($item) => $item->date, fn($item) => $item->gender]),
 
             'completions' => LessonProgress::selectRaw(
-                'users.gender, DATE(lesson_progress.created_at) as date, COUNT(*) as total_completions'
+                'users.gender, DATE(enrolls.created_at) as date, COUNT(*) as total_completions'
             )
-                ->join('users', 'users.id', '=', 'lesson_progress.user_id')
-                ->whereBetween('lesson_progress.created_at', [$start, $end])
-                ->where('lesson_progress.is_passed', config('common.confirmation.yes'))
+                ->join('users', 'users.id', '=', 'enrolls.user_id')
+                ->whereBetween('enrolls.created_at', [$start, $end])
+                ->where('enrolls.is_passed', config('common.confirmation.yes'))
                 ->groupBy(['date', 'gender'])
-                ->get()->groupBy([fn ($item) => $item->date, fn ($item) => $item->gender]),
+                ->get()->groupBy([fn($item) => $item->date, fn($item) => $item->gender]),
 
             'students' => User::selectRaw('gender, DATE(created_at) as date, COUNT(*) as total_students')
                 ->whereBetween('created_at', [$start, $end])
                 ->whereNull('deleted_at')
                 ->groupBy(['date', 'gender'])
-                ->get()->groupBy([fn ($item) => $item->date, fn ($item) => $item->gender]),
+                ->get()->groupBy([fn($item) => $item->date, fn($item) => $item->gender]),
         ];
     }
 
     private function save(string $startDate, string $endDate, array $reportData): void
     {
         $dateRange = collect(Carbon::parse($startDate)->daysUntil(Carbon::parse($endDate)))
-            ->map(fn ($d) => $d->toDateString());
+            ->map(fn($d) => $d->toDateString());
 
         foreach ($dateRange as $date) {
             foreach (config('common.gender') as $gender) {
