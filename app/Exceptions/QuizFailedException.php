723386e60b6309b9c28a64e4059e466572cbbd57
Sajal Ahmed <sajal.ahmed@brainstation-23.com>
2025-04-22 11:36:46 +0600

    refactor with constructor promitoins

--- app/Exceptions/QuizFailedException.php
+++ app/Exceptions/QuizFailedException.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Illuminate\Http\Response;
-use Exception;
 
-class QuizFailedException extends Exception
+class QuizFailedException extends \Exception
 {
     public function __construct(
         string $message = '',
         public array $data = [],
-        int $statusCode = Response::HTTP_FORBIDDEN
+        int $statusCode = Response::HTTP_FORBIDDEN,
     ) {
         parent::__construct($message, $statusCode);
     }
